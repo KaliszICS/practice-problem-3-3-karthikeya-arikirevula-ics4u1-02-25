a3b4e360e0f2513f20dc20cedf05eec82dbef322
Karthikeya Arikirevula <karthikeya.arikirevula@student.tdsb.on.ca>
2025-04-07 21:21:38 +0000

    passed

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -6,4 +6,44 @@ public class PracticeProblem {
 
 	}
 
+		public static ArrayList<String> perms(String word) {
+
+		ArrayList<String> permu = new ArrayList<String>();
+
+		permHelper("", word, permu);
+		return permu;
+	}
+
+	public static void permHelper(String usedLetters, String unusedLetters, ArrayList<String> permu) {
+if(unusedLetters.isEmpty()){
+	permu.add(usedLetters);
+	return;
+}
+for(int i = 0; i < unusedLetters.length(); i++){
+	permHelper(usedLetters + unusedLetters.charAt(i), unusedLetters.substring(0,i) + unusedLetters.substring(i+1), permu);
+}
+	}
+
+
+	public static ArrayList<String> permsUnique(String word) {
+
+		ArrayList<String> permu = new ArrayList<String>();
+
+		permUniqueHelper("", word, permu);
+		return permu;
+	}
+
+	public static void permUniqueHelper(String usedLetters, String unusedLetters, ArrayList<String> permu) {
+if(unusedLetters.isEmpty()){
+	if(!(permu.contains(usedLetters))){
+		permu.add(usedLetters);
+		return;
+} 
+return;
+}
+for(int i = 0; i < unusedLetters.length(); i++){
+	permUniqueHelper(usedLetters + unusedLetters.charAt(i), unusedLetters.substring(0,i) + unusedLetters.substring(i+1), permu);
+}
+	}
+
 }
